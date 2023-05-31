aff88585b6f779fa135374d4ddb064f6ed7703e4
shivanisah <shivanisah145@gmail.com>
2023-05-31 09:26:47 +0545

    uiChanges

--- lib/screens/auth_screens/login.dart
+++ lib/screens/auth_screens/login.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:tutor_app/FirstScreen/appBar.dart';
+import 'package:tutor_app/screens/auth_screens/teacherSignUpscreen.dart';
+import 'package:tutor_app/utils/colors.dart';
 
 import '../../providers/auth_provider.dart';
 
@@ -36,10 +40,11 @@ double width = MediaQuery.of(context).size.width;
 double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar:PreferredSize(
-        preferredSize:const Size.fromHeight(60),
-        child:AppB(),
-      ),
+      appBar:AppBar(),
+      // appBar:PreferredSize(
+      //   preferredSize:const Size.fromHeight(60),
+      //   child:AppB(),
+      // ),
       body:Container(  
       
         height:height,
@@ -50,35 +55,69 @@ double height = MediaQuery.of(context).size.height;
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [            
-              SizedBox(height:20),
-              Center(
-                child: Text("Login", 
-                // textAlign: TextAlign.right,
-                style:TextStyle(fontSize:20 )),
-              ),
+              children: [ 
+            SizedBox(height:90),
+            Text(
+               'Log In',
+               style:  GoogleFonts.poppins(
+
+              fontSize:  25,
+                  fontWeight:  FontWeight.w500,
+                  height:  1.5,
+                color:  Color(0xff000000),
+
+                )
+            ),
+            SizedBox(height:6),
+            Text(
+               'Please enter your login details',
+               style:  GoogleFonts.poppins(
+
+              fontSize:  12,
+                  // fontWeight:  FontWeight.w600,
+                  height:  1.5,
+                color:  Color(0xff000000),
+
+                )
+            ),           
+           
                  SizedBox(height: 20),
+                             Text(
+               'Email Address ',
+               style:  GoogleFonts.poppins(
+
+              fontSize:  15,
+                  // fontWeight:  FontWeight.w600,
+                  height:  1.5,
+                color:  Colors.black,
+
+                )
+            ),           
+              SizedBox(height:4),
                      TextFormField(              
                 controller:emailController,
                 decoration:InputDecoration(             
                 border:InputBorder.none,  
-                labelText:"Email",
-                labelStyle: TextStyle(color:Colors.black),
+                hintText:"Email",
+                hintStyle: TextStyle(color:Colors.black,fontWeight:FontWeight.w400 
+                ),
+                fillColor: Color.fromARGB(255, 234, 235, 236),
+                filled: true,
                 errorBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.red,),
-                  borderRadius:BorderRadius.circular(12),
+                  borderRadius:BorderRadius.circular(6),
                 ),
                 focusedErrorBorder: OutlineInputBorder
                 (borderSide:BorderSide(color:Colors.red),
-                borderRadius:BorderRadius.circular(12),
+                borderRadius:BorderRadius.circular(6),
                 ),
                 enabledBorder:OutlineInputBorder(                 
-                    borderRadius:BorderRadius.circular(12),
-                    borderSide:BorderSide(color: Color.fromARGB(255, 0, 0, 0)), 
+                    borderRadius:BorderRadius.circular(6),
+                    borderSide:BorderSide(color: Color.fromARGB(255, 234, 235, 236)), 
                   ),
                   focusedBorder:OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide:BorderSide(color:Colors.black)
+                  borderRadius: BorderRadius.circular(6),
+                    borderSide:BorderSide(color: Palette.theme), 
                   )
                     
                 ),
@@ -94,7 +133,20 @@ double height = MediaQuery.of(context).size.height;
                     return null;
                   },  
                  ),
-                    SizedBox(height:20),
+               SizedBox(height: 15),
+              Text(
+               'Password ',
+               style:  GoogleFonts.poppins(
+
+              fontSize:  15,
+                  // fontWeight:  FontWeight.w600,
+                  height:  1.5,
+                color:  Colors.black,
+
+                )
+            ),   
+            SizedBox(height:4) ,       
+
               TextFormField(    
                 obscureText:_isObscure,          
                 controller:passwordController,
@@ -112,23 +164,26 @@ double height = MediaQuery.of(context).size.height;
           
                 
                 border:InputBorder.none,  
-                labelText:"Password",
-                labelStyle: TextStyle(color:Colors.black),
+                hintText:"Password",
+                hintStyle: TextStyle(color:Colors.black,fontWeight:FontWeight.w400),
+                fillColor: Color.fromARGB(255, 234, 235, 236),
+                filled: true,
+
                 errorBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.red,),
-                  borderRadius:BorderRadius.circular(12),
+                  borderRadius:BorderRadius.circular(6),
                 ),
                 focusedErrorBorder: OutlineInputBorder
                 (borderSide:BorderSide(color:Colors.red),
-                borderRadius:BorderRadius.circular(12),
+                borderRadius:BorderRadius.circular(6),
                 ),
                 enabledBorder:OutlineInputBorder(                 
-                    borderRadius:BorderRadius.circular(12),
-                    borderSide:BorderSide(color: Color.fromARGB(255, 0, 0, 0)), 
+                    borderRadius:BorderRadius.circular(6),
+                    borderSide:BorderSide(color: Color.fromARGB(255, 234, 235, 236)), 
                   ),
                   focusedBorder:OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide:BorderSide(color:Colors.black)
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide:BorderSide(color:Palette.theme)
                   )
                     
                 ),
@@ -145,31 +200,92 @@ double height = MediaQuery.of(context).size.height;
                             },
             
                  ),
-          
-                 SizedBox(height:20),
-                        Center(
-                  child:      MaterialButton(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(20.0))),
-                                elevation: 5.0,
-                                height: 40,
-                                onPressed: () {
+
+                 SizedBox(height:40),
+                 Center(
+                   child: RichText(
+                     text: TextSpan(
+                         style: TextStyle(color: Colors.black),
+                         children: <TextSpan>[
+                           TextSpan(text: 'Forgot Password? ',
+                           style:GoogleFonts.poppins(
+
+                          fontSize:  15,
+                          fontWeight:  FontWeight.w400,
+                          // height:  1.5,
+                          color:  Color.fromARGB(255, 134, 148, 162),
+
+                )
+                           ),
+                           TextSpan(text: 'Reset Password',
+                           style:GoogleFonts.poppins(
+
+                          fontSize:  15,
+                          fontWeight:  FontWeight.w600,
+                          // height:  1.5,
+                          color:  Palette.theme1,
+
+                              )
+                            ),
+                         ],
+                     ),
+                   ),
+                 ),
+                SizedBox(height:20),
+                    GestureDetector(
+                                      onTap: (){
                                   if(_formkey.currentState!.validate()){
                                       provider.userLogin(context,emailController.text.toString(), passwordController.text.toString());
                                   }
-                                   
-                                },
 
-                                child: provider.loading?CircularProgressIndicator():Text(
-                                  "Login",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                ),
-                                color: Colors.white,
-                              ),
-                        ),
+                                      },
+                                      child: Container(
+                                        height:50,
+                                        width:350,
+                                        padding: EdgeInsets.all(5),
+                                        decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(6),
+                                        // border: Border.all(width: 0.7,color: Colors.black),
+                                          color: Palette.theme1
+                                        ),
+                                        child:provider.loading?Center(child: CircularProgressIndicator()):            
+                                            Center(child: Text('Continue',style:TextStyle(color:Colors.white,fontSize: 16))),                                       
+                                      ),
+                                    ),
+
+              SizedBox(height:20),
+                               Center(
+                   child: RichText(
+                     text: TextSpan(
+                         style: TextStyle(color: Colors.black),
+                         children: <TextSpan>[
+                           TextSpan(text: "Don't have an account? ",
+                           style:GoogleFonts.poppins(
+
+                          fontSize:  15,
+                          fontWeight:  FontWeight.w400,
+                          // height:  1.5,
+                          color:  Color.fromARGB(255, 134, 148, 162),
+
+                )
+                           ),
+                           TextSpan(text: 'Signup',
+                           recognizer:TapGestureRecognizer()..onTap = () =>
+                           Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) =>TutorRegistration(),)),
+                           style:GoogleFonts.poppins(
+
+                          fontSize:  15,
+                          fontWeight:  FontWeight.w600,
+                          // height:  1.5,
+                          color:  Palette.theme1,
+
+                )
+                            ),
+                         ],
+                     ),
+                   ),
+                 ),
+
 
                     
             ],),
